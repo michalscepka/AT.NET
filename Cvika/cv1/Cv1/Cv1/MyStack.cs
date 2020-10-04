cfe0de6a438cf7a386f8d3625907e43704375d12
michalscepka <scepkamichal1@gmail.com>
2020-10-05 00:32:04 +0200

    cv1 init

---
+++ Cvika/cv1/Cv1/Cv1/MyStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cv1
+{
+	delegate void StackEventHandler();
+
+	class EmptyStackException : Exception { }
+
+	class MyStack<T> : IEnumerable<T>
+	{
+		private T[] data;
+		private int index;
+
+		//"event" zaridi ze se nebude moct volat z jinych trid nez je tato
+		public event StackEventHandler OnPush;
+		public event StackEventHandler OnPop;
+
+		public int Top
+		{
+			get
+			{
+				if (this.index == -1)
+					throw new EmptyStackException();
+				else
+					return this.index;
+			}
+		}
+
+		public MyStack()
+		{
+			data = new T[10];
+			index = -1;
+		}
+
+		public void Push(T new_element)
+		{
+			data[++index] = new_element;
+			OnPush?.Invoke();
+		}
+
+		public T Pop()
+		{
+			OnPop?.Invoke();
+			return data[index--];
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			for(int i = index; i >= 0; i--)
+			{
+				yield return data[i];
+			}
+		}
+
+		// druha metoda, kvuli starym verzim .NET
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			for (int i = index; i >= 0; i--)
+			{
+				yield return data[i];
+			}
+		}
+	}
+}
---
+++ Cvika/cv1/Cv1/Cv1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cv1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyStack<int> st = new MyStack<int>();
+
+			st.OnPush += () => Console.WriteLine("Pushed");
+			st.OnPop += () => Console.WriteLine("Poped");
+
+			//Console.WriteLine("Top: " + st.Top);
+			st.Push(1);
+			Console.WriteLine("Top: " + st.Top);
+			st.Push(2);
+			Console.WriteLine("Top: " + st.Top);
+
+			foreach(int x in st)
+			{
+				Console.WriteLine(x);
+			}
+
+			Console.WriteLine("Pop: " + st.Pop());
+
+			Console.WriteLine("Top: " + st.Top);
+			Console.WriteLine("Pop: " + st.Pop());
+
+			//Console.WriteLine("Top: " + st.Top);
+			//Console.WriteLine(st.Pop());
+		}
+	}
+}
